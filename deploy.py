0fe5504348e199c89d74db6e974d90f747c60b34
AlexisMerienne <alexis.merienne@gmail.com>
2022-04-07 14:45:14 +0200

    Add enquete to deploy

--- deploy.py
+++ deploy.py
@@ -11,12 +11,16 @@ if __name__ == '__main__':
     original_bc = r'build/bilancarbone.html'
     target_bc = r'output/views/bilancarbone.html'
 
+    original_enq = r'build/enquete.html'
+    target_enq = r'output/views/enquete.html'
+
     original_assets = 'build/assets'
     target_assets = 'output/assets'
 
     shutil.copyfile(original_ac, target_ac)
     shutil.copyfile(original_home, target_home)
     shutil.copyfile(original_bc, target_bc)
+    shutil.copyfile(original_enq, target_enq)
     copy_tree(original_assets, target_assets)
 
 
